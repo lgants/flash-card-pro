b4887160756d5633ded9839e8344cdd46eae67ae
lgants <lgants@gmail.com>
2017-08-19 12:18:43 -0500

    connected StackList component with redux

--- src/components/StackList.jsx
+++ src/components/StackList.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 import stacks from '../data/stacks.json';
+import { setStack } from '../actions';
 
 class StackList extends Component {
   render() {
@@ -9,8 +12,11 @@ class StackList extends Component {
         {
           stacks.map(stack => {
             return (
-              <Link to="/stack">
-                <h4 key={stack.id}>{stack.title}</h4>
+              <Link
+                key={stack.id}
+                to="/stack"
+                onClick={() => this.props.setStack(stack)}>
+                <h4>{stack.title}</h4>
               </Link>
             )
           })
@@ -20,4 +26,17 @@ class StackList extends Component {
   }
 }
 
-export default StackList;
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ setStack }, dispatch);
+// }
+// const connectComponent = connect(null, mapDispatchToProps);
+// export default connectComponent(StackList);
+
+// NOTE 1st refactor: condensed export to one line
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ setStack }, dispatch);
+// }
+// export default connect(null, mapDispatchToProps)(StackList);
+
+// NOTE 2nd refactor: used react-redux shortcut to directly pass in bound object action creators as 2nd parameter to connect function and bind them right away
+export default connect(null, { setStack })(StackList);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import { setStack } from './actions';
 import App from './components/App';
 import Stack from './components/Stack';
 
+const store = createStore(rootReducer);
+store.subscribe(() => {
+  console.log('store', store.getState());
+})
+store.dispatch(setStack({ id: 0, title: 'example', cards: [] }))
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App}/>
-      <Route exact path="/stack" component={Stack}/>
-    </Switch>
-  </BrowserRouter>, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App}/>
+        <Route exact path="/stack" component={Stack}/>
+      </Switch>
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
